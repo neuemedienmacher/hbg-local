0a9abcc2d5439692b1a2d10f7354a999793f1cf0
Konstantin K <konstantin.koss@gmail.com>
2018-02-02 09:56:03 +0100

    remove production redirect

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,18 +1,18 @@
-case Rails.env
-when 'production'
-  Rails.application.configure do
-    config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-      r301 %r{.*}, 'http://www.clarat.org$&', if: Proc.new { |rack_env|
-        rack_env['SERVER_NAME'] != 'www.clarat.org'
-      }
-    end
-  end
-when 'staging'
-  Rails.application.configure do
-    config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-      r301 %r{.*}, 'http://staging.clarat.org$&', if: Proc.new { |rack_env|
-        rack_env['SERVER_NAME'] != 'staging.clarat.org'
-      }
-    end
-  end
-end
+# case Rails.env
+# when 'production'
+#   Rails.application.configure do
+#     config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+#       r301 %r{.*}, 'http://local.handbookgermany.de$&', if: Proc.new { |rack_env|
+#         rack_env['SERVER_NAME'] != 'local.handbookgermany.de'
+#       }
+#     end
+#   end
+# when 'staging'
+#   Rails.application.configure do
+#     config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+#       r301 %r{.*}, 'http://staging.clarat.org$&', if: Proc.new { |rack_env|
+#         rack_env['SERVER_NAME'] != 'staging.clarat.org'
+#       }
+#     end
+#   end
+# end
