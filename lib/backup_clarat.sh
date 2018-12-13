575ebf152cd3a1c941ab8220160298ae53d2b87c
Nils Vollmer <nils.vollmer@quandoo.com>
2018-12-13 09:18:11 +0100

    adjusting backup script

--- lib/backup_clarat.sh
+++ lib/backup_clarat.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 /app/vendor/heroku-toolbelt/bin/heroku pg:backups capture --app hbg-local
 export CURRENT_TIME=`date +"%Y-%m-%d-%H-%M-%S"`
-curl -o /tmp/hbg-local-$CURRENT_TIME.dump `/app/vendor/heroku-toolbelt/bin/heroku pg:backups public-url --app hbg-local`
+export BACKUP_PUBLIC_URL=`/app/vendor/heroku-toolbelt/bin/heroku pg:backups public-url --app hbg-local`
+curl -o /tmp/hbg-local-$CURRENT_TIME.dump $BACKUP_PUBLIC_URL
 sftp -P 8022 -o "IdentityFile=/app/.ssh/id_rsa" hbglocal@$SFTP_IP:backups <<COMMAND
   put /tmp/hbg-local-$CURRENT_TIME.dump
   quit
