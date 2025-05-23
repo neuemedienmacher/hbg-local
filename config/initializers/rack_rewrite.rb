31d313f2fffc411c85dd601417e589bf610abbb6
Robert Lanzke <winkelement@hotmail.com>
2025-05-23 14:50:14 +0200

    change url

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -2,7 +2,7 @@ case Rails.env
 when 'production'
   Rails.application.configure do
     config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-      r301 %r{.*}, 'http://local.handbookgermany.de$&', if: Proc.new { |rack_env|
+      r301 %r{.*}, 'https://hbg-local.herokuapp.com$&', if: Proc.new { |rack_env|
         rack_env['SERVER_NAME'] != 'local.handbookgermany.de'
       }
     end
